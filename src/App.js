0697237f1a22a71af4a396ed6c79907b3680d62d
arupdawn <rupchandradawn@gmail.com>
2021-12-21 13:21:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const diffDays = () => {
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const firstDate = new Date(1640048442000); // 7th Dec 2021
+    console.log("firstDate >>", firstDate);
+    const secondDate = new Date(Date.now());
+    console.log("secondDate >>", secondDate);
+    const diffDays =
+      Math.round(Math.abs((firstDate - secondDate) / oneDay)) + 1;
+    return diffDays;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="OuterContainer">
+
+        <div className="eachChallenge">
+          <div className="challengeNo">1.) </div>
+          <div className="challengeName">#90DaysReadingChallenge</div>
+          <div className="dayNo">Day : {diffDays()}</div>
+        </div>
+
+        {/* <div className="eachChallenge">
+          <div className="challengeNo">1.) </div>
+          <div className="challengeName">#90DaysWritingChallenge.......................................</div>
+          <div className="dayNo">Day : {diffDays()}</div>
+        </div> */}
+
+      </div>
     </div>
   );
 }
